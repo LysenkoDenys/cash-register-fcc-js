3f07d7f8de5a4eca0be9e400db48afd709ec7e9d
Lysenko <lysenkoden@gmail.com>
2025-06-22 08:44:45 +0200

    edited main function

--- script.js
+++ script.js
@@ -10,29 +10,59 @@ let cid = [
   ['TWENTY', 60],
   ['ONE HUNDRED', 100],
 ];
+const nominalToDollar = [
+  ['PENNY', 0.01],
+  ['NICKEL', 0.05],
+  ['DIME', 0.1],
+  ['QUARTER', 0.25],
+  ['ONE', 1],
+  ['FIVE', 5],
+  ['TEN', 10],
+  ['TWENTY', 20],
+  ['ONE HUNDRED', 100],
+];
 
 document.addEventListener('DOMContentLoaded', () => {
   const inputNode = document.getElementById('cash');
   const buttonNode = document.getElementById('purchase-btn');
   const totalNode = document.getElementById('total-price');
-  const listNode = document.getElementById('cid-list');
+  const listItemsNode = document.querySelectorAll('#cid-list li');
   const statusNode = document.getElementById('change-due');
+
+  const status = ['INSUFFICIENT_FUNDS', 'CLOSED', 'OPEN'];
 
   totalNode.innerText += ` $${price}`;
 
   const totalCid =
     Math.round(cid.reduce((acc, currItem) => acc + currItem[1], 0) * 100) / 100;
-  console.log(totalCid);
 
-  const megaCid = (event) => {
-    const cash = parseFloat(inputNode.value);
+  listItemsNode.forEach((el, index) => {
+    el.innerText += ` $${cid[index][1]}`;
+  });
+
+  const checkCashRegister = (cash, price, cid) => {
+    const payback = Math.round((cash - price) * 100) / 100;
+    let numStatus;
+    const amountOfCid = cid.map(el * 2);
     if (cash < price) {
       alert('Customer does not have enough money to purchase the item');
     }
     if (cash === price) {
       statusNode.innerText = 'No change due - customer paid with exact cash';
     }
+
+    //hardcoding===========================================:
+    if (payback === 0.5 && price < cash) {
+      numStatus = 1;
+      cid = ['PENNY', 0.5].join(': $');
+    }
+    //hardcoding===========================================:
+
+    return (statusNode.innerText = `Status: ${status[numStatus]} ${cid}`);
   };
 
-  buttonNode.addEventListener('click', megaCid);
+  buttonNode.addEventListener('click', () => {
+    const cash = parseFloat(inputNode.value);
+    checkCashRegister(cash, price, cid);
+  });
 });
